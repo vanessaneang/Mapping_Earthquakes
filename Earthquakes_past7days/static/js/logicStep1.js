eaf4a81a8ca73db314c4deb4f6e8a8af81197fa1
Vanessa Neang <neangvanessa@gmail.com>
2022-11-22 18:11:17 -0800

    mapping_earthquakes_7days

--- Earthquakes_past7days/static/js/logicStep1.js
+++ Earthquakes_past7days/static/js/logicStep1.js
@@ -30,36 +30,16 @@ let baseMaps = {
 
 // Create the map object with center, zoom level and default layer.
 let map = L.map('mapid', {
-    center: [43.7, -79.3],
+    center: [39.5, -98.5],
     zoom: 3,
     layers: [streets]
 })
 
 // Pass our map layers into our layers control and add the layers control to the map.
 L.control.layers(baseMaps).addTo(map);
-
-// Accessing the Toronto airline routes GeoJSON URL.
-let torontoHoods = "https://raw.githubusercontent.com/vanessaneang/Mapping_Earthquakes/main/torontoNeighborhoods.json"
-
-// Then we add our 'graymap' tile layer to the map.
-streets.addTo(map);
-
-// Create myStyle for map
-let myStyle = {
-  weight: 1,
-  color: "blue",
-  fillColor: "#ffffa1",
-};
   
-
-// Grabbing our GeoJSON data.
-d3.json(torontoHoods).then(function(data) {
-  console.log(data);
-// Creating a GeoJSON layer with the retrieved data.
-L.geoJSON(data, {
-  style: myStyle,
-  onEachFeature: function(feature, layer) {
-    layer.bindPopup("<h3> Neighborhood: " + feature.properties.AREA_NAME + "</h3>");
-  }
-}).addTo(map);
+// Retrieve the earthquake GeoJSON data.
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
+  // Creating a GeoJSON layer with the retrieved data.
+  L.geoJSON(data).addTo(map);
 });
